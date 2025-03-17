11bd2834673e4112369155616a7e5fcd366698bb
Shalu004 <mushaluthakur785@gmail.com>
2025-03-17 12:42:34 +0000

    config

--- DAY01/basics.js
+++ DAY01/basics.js
@@ -1,1 +1,49 @@
-console.log("Shalu")
+//console.log("Shalu")
+
+const accountId="1234abc"
+let accountemail="abc@gmail.com"
+var password="123abc"
+accountcity="patna"
+
+accountcity="delhi"
+password="abc123"
+
+//const cannot be changed
+
+/*
+var is not prefferd due to functional scope issue
+let is used
+*/
+
+console.table([accountId, accountemail, password, accountcity])    //to print all data intabular form
+
+
+
+"use strict";  //to use all newer versions of js
+
+//alert(3+3)  //  Error- as we are not using browser here
+
+let name="shalu"
+let age=19
+let athome=false
+
+/*
+primitive data-type
+
+int   (2^53 size)
+bigint
+string
+boolean=> true/false
+null=> standalone value
+undefined=> value not assign
+symbol=> unique
+
+non-primitive datatype
+object
+*/
+
+console.log(typeof null)     //  object!!
+console.log(typeof undefined)   // undefined
+
+
+
